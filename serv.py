8904a680bd1a1ecc505e852b639d4cc147ace47a
Arvin Ding <dingarvin@gmail.com>
2018-12-25 20:53:49 -0800

    Update

--- serv.py
+++ serv.py
@@ -1,20 +1,38 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
-
-class Serv(BaseHTTPRequestHandler):
-
+import os
+ 
+ 
+class StaticServer(BaseHTTPRequestHandler):
+ 
     def do_GET(self):
+        root = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Website')
+        print(root)
         if self.path == '/':
-            self.path = '/index.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
+            filename = root + '/index.html'
+        else:
+            filename = root + self.path
+ 
+        self.send_response(200)
+        if filename[-4:] == '.css':
+            self.send_header('Content-type', 'text/css')
+        elif filename[-5:] == '.json':
+            self.send_header('Content-type', 'application/javascript')
+        elif filename[-3:] == '.js':
+            self.send_header('Content-type', 'application/javascript')
+        elif filename[-4:] == '.ico':
+            self.send_header('Content-type', 'image/x-icon')
+        else:
+            self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
-
-
-httpd = HTTPServer(('localhost', 8080), Serv)
-httpd.serve_forever()
+        with open(filename, 'rb') as fh:
+            html = fh.read()
+            #html = bytes(html, 'utf8')
+            self.wfile.write(html)
+ 
+def run(server_class=HTTPServer, handler_class=StaticServer, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd on port {}'.format(port))
+    httpd.serve_forever()
+ 
+run()
